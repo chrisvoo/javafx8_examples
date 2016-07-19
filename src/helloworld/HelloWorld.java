d6ab548f5d8648b201cbf5b16ede85321e71ab7a
ccastelli <ccastelli@daneel>
2016-07-19 23:28:12 +0200

    Menus, ListView, ObservableLists

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -11,7 +11,11 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -19,7 +23,13 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Reflection;
@@ -47,7 +57,16 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.FlowPane;
@@ -847,6 +866,211 @@ public class HelloWorld extends Application {
         
         primaryStage.setScene(scene);
         
+        primaryStage.show();
+    }
+    
+    /**
+     * Menu example.
+     * getImtems(): collections that have the ability to notify and update UI 
+     * controls as items are added or removed.
+     * <b>keyboard mnemonics</b>: Alt + first letter after "_"
+     * <b>keyboard combinations</b>: shortcuts such as Ctrl + S (Windows) or Command + S (Mac)
+     * @param primaryStage 
+     */
+    public void menu(Stage primaryStage) {
+        primaryStage.setTitle("Menus Example");
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 300, 250, Color.WHITE);
+        
+        final StringProperty statusProperty = new SimpleStringProperty();
+
+        InnerShadow iShadow = new InnerShadow();
+        iShadow.setOffsetX(3.5f);
+        iShadow.setOffsetY(3.5f);
+        final Text status = new Text();
+        status.setEffect(iShadow);
+        status.setX(100);
+        status.setY(50);
+        status.setFill(Color.LIME);
+        status.setFont(Font.font(null, FontWeight.MEDIUM, 18));
+        
+        // the label will change consistently with status string property
+        status.textProperty().bind(statusProperty);
+        statusProperty.set("Keyboard Shortcuts\n"
+                + "Ctrl-N, \n"
+                + "Ctrl-S, \n"
+                + "Ctrl-X \n"
+                + "Ctrl-Shift-E");
+        root.setCenter(status);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+        root.setTop(menuBar); 
+
+        // File menu - new, save, exit
+        Menu fileMenu = new Menu("_File");
+        MenuItem newMenuItem = new MenuItem("_New");
+        newMenuItem.setOnAction(actionEvent -> statusProperty.set("Ctrl-N"));
+        
+        MenuItem saveMenuItem = new MenuItem("Save");
+        saveMenuItem.setMnemonicParsing(true); // comination
+        saveMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.S, 
+                                    KeyCombination.SHORTCUT_DOWN));
+        saveMenuItem.setOnAction(actionEvent -> statusProperty.set("Ctrl-S"));
+        
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.X, 
+                                    KeyCombination.SHORTCUT_DOWN));
+        exitMenuItem.setOnAction(actionEvent -> {
+            statusProperty.set("Ctrl-X");
+            Platform.exit();
+        });
+        
+        scene.getAccelerators().put(
+                new KeyCodeCombination(KeyCode.E, 
+                                       KeyCombination.SHORTCUT_DOWN, 
+                                       KeyCombination.SHIFT_DOWN),
+                () -> statusProperty.set("Ctrl-Shift-E")
+        );
+        
+        // if you click with mouse right button on every part of the scene but the menu,
+        // it will appear a context menu with Exit menu item
+        ContextMenu contextFileMenu = new ContextMenu(exitMenuItem);
+
+        primaryStage.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent me) -> {
+            if (me.getButton() == MouseButton.SECONDARY) {
+                contextFileMenu.show(root, me.getScreenX(), me.getScreenY());
+            } else {
+                contextFileMenu.hide();
+            }
+        });
+
+        fileMenu.getItems().addAll(newMenuItem, 
+                saveMenuItem, 
+                new SeparatorMenuItem(), 
+                exitMenuItem
+        );
+        
+        // Cameras menu - camera 1, camera 2
+        Menu cameraMenu = new Menu("_Cameras");
+        CheckMenuItem cam1MenuItem = new CheckMenuItem("Show Camera 1");
+        cam1MenuItem.setSelected(true);
+        cameraMenu.getItems().add(cam1MenuItem);
+
+        CheckMenuItem cam2MenuItem = new CheckMenuItem("Show Camera 2");
+        cam2MenuItem.setSelected(true);
+        cameraMenu.getItems().add(cam2MenuItem);
+
+
+        // Alarm menu
+        Menu alarmMenu = new Menu("_Alarm");
+
+        // sound or turn alarm off, one exclude the other
+        ToggleGroup tGroup = new ToggleGroup();
+        RadioMenuItem soundAlarmItem = new RadioMenuItem("Sound Alarm"); 
+        soundAlarmItem.setToggleGroup(tGroup);
+
+        RadioMenuItem stopAlarmItem = new RadioMenuItem("Alarm Off");
+        stopAlarmItem.setToggleGroup(tGroup);
+        stopAlarmItem.setSelected(true);
+
+        alarmMenu.getItems().addAll(
+                soundAlarmItem, 
+                stopAlarmItem, 
+                new SeparatorMenuItem());
+
+        Menu contingencyPlans = new Menu("Contingent Plans");
+        contingencyPlans.getItems().addAll(
+                new CheckMenuItem("Self Destruct in T minus 50"),
+                new CheckMenuItem("Turn off the coffee machine "),
+                new CheckMenuItem("Run for your lives! "));
+
+        alarmMenu.getItems().add(contingencyPlans);
+
+        menuBar.getMenus().addAll(fileMenu, cameraMenu, alarmMenu);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    /**
+     * Picklists examples with ObservableLists. It's enough to remove or add 
+     * items to these lists for refreshing UI
+     * @param primaryStage 
+     */
+    public void observableList(Stage primaryStage) {
+        primaryStage.setTitle("Hero Picker: Working with ObservableLists");
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 400, 250, Color.WHITE);
+
+        // create a grid pane
+        GridPane gridpane = new GridPane();
+        gridpane.setPadding(new Insets(5));
+        gridpane.setHgap(10);
+        gridpane.setVgap(10);
+        ColumnConstraints column1 = new ColumnConstraints(150, 150, Double.MAX_VALUE);
+        ColumnConstraints column2 = new ColumnConstraints(50);
+        ColumnConstraints column3 = new ColumnConstraints(150, 150, Double.MAX_VALUE);
+        column1.setHgrow(Priority.ALWAYS);
+        column3.setHgrow(Priority.ALWAYS);
+        gridpane.getColumnConstraints().addAll(column1, column2, column3); 
+
+        // Candidates label
+        Label candidatesLbl = new Label("Candidates");
+        GridPane.setHalignment(candidatesLbl, HPos.CENTER);
+        gridpane.add(candidatesLbl, 0, 0);
+
+        // Heroes label
+        Label heroesLbl = new Label("Heroes");
+        gridpane.add(heroesLbl, 2, 0);
+        GridPane.setHalignment(heroesLbl, HPos.CENTER);
+
+        // Candidates
+        final ObservableList<String> candidates = 
+                FXCollections.observableArrayList(
+                "Superman", "Spiderman", "Wolverine",
+                "Police", "Fire Rescue", "Soldiers", 
+                "Dad & Mom", "Doctor", "Politician", 
+                "Pastor", "Teacher"
+        );
+        final ListView<String> candidatesListView = new ListView<>(candidates);
+        gridpane.add(candidatesListView, 0, 1);
+
+        // heros
+        final ObservableList<String> heroes = FXCollections.observableArrayList();
+        final ListView<String> heroListView = new ListView<>(heroes);
+        gridpane.add(heroListView, 2, 1);
+
+        // select heroes
+        Button sendRightButton = new Button(" > ");
+        sendRightButton.setOnAction((ActionEvent event) -> {
+            String potential = candidatesListView.getSelectionModel().getSelectedItem();
+            if (potential != null) {
+                candidatesListView.getSelectionModel().clearSelection();
+                candidates.remove(potential);
+                heroes.add(potential);
+            }
+        });
+
+        // deselect heroes
+        Button sendLeftButton = new Button(" < ");
+        sendLeftButton.setOnAction((ActionEvent event) -> {
+            String notHero = heroListView.getSelectionModel().getSelectedItem();
+            if (notHero != null) {
+                heroListView.getSelectionModel().clearSelection();
+                heroes.remove(notHero);
+                candidates.add(notHero);
+            }
+        });
+
+        VBox vbox = new VBox(5);
+        vbox.getChildren().addAll(sendRightButton,sendLeftButton);
+
+        gridpane.add(vbox, 1, 1);
+        root.setCenter(gridpane);
+
+        GridPane.setVgrow(root, Priority.ALWAYS);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
